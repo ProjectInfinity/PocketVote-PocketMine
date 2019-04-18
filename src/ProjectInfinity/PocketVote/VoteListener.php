7a728627248de18c5e9c04a10d94801084179ade
ProjectInfinity <a.sjur91@gmail.com>
2019-04-18 03:39:57 +0200

    Add GUI for first time setup.

--- src/ProjectInfinity/PocketVote/VoteListener.php
+++ src/ProjectInfinity/PocketVote/VoteListener.php
@@ -2,9 +2,13 @@
 
 namespace ProjectInfinity\PocketVote;
 
+use jojoe77777\FormAPI\CustomForm;
+use jojoe77777\FormAPI\FormAPI;
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\event\Listener;
 use pocketmine\event\player\PlayerJoinEvent;
+use pocketmine\Player;
+use pocketmine\utils\TextFormat;
 use ProjectInfinity\PocketVote\event\VoteDispatchEvent;
 use ProjectInfinity\PocketVote\event\VoteEvent;
 
@@ -27,9 +31,7 @@ class VoteListener implements Listener {
 
         $sender = new ConsoleCommandSender();
         foreach($this->plugin->cmds as $cmd) {
-            $cmd = str_replace('%player', $event->getPlayer(), $cmd);
-            $cmd = str_replace('%ip', $event->getIp(), $cmd);
-            $cmd = str_replace('%site', $event->getSite(), $cmd);
+            $cmd = str_replace(['%player', '%ip', '%site'], [$event->getPlayer(), $event->getIp(), $event->getSite()], $cmd);
             $this->plugin->getServer()->dispatchCommand($sender, $cmd);
         }
         
@@ -40,9 +42,7 @@ class VoteListener implements Listener {
         }
 
         foreach($this->plugin->cmdos as $cmd) {
-            $cmd = str_replace('%player', $event->getPlayer(), $cmd);
-            $cmd = str_replace('%ip', $event->getIp(), $cmd);
-            $cmd = str_replace('%site', $event->getSite(), $cmd);
+            $cmd = str_replace(['%player', '%ip', '%site'], [$event->getPlayer(), $event->getIp(), $event->getSite()], $cmd);
             $this->plugin->getServer()->dispatchCommand($sender, $cmd);
         }
         
@@ -53,6 +53,31 @@ class VoteListener implements Listener {
      * @param PlayerJoinEvent $event
      */
     public function onPlayerJoin(PlayerJoinEvent $event) {
+        // Check if identity and secret has been set. If not show GUI.
+        if($this->plugin->identity === '' && $this->plugin->secret === '' && $event->getPlayer()->isOp()) {
+            $form = new CustomForm(static function(Player $player, ?array $data) {
+                if(!$data) {
+                    $player->sendMessage(TextFormat::YELLOW.'[PocketVote] OK! We\'ll remind you next time you log in.');
+                    return;
+                }
+                if($data['secret'] === '' || $data['identity'] === '') {
+                    $player->sendMessage(TextFormat::RED.'Make sure you entered your secret and identity correctly.');
+                    return;
+                }
+                $localPlugin = PocketVote::getPlugin();
+                $localPlugin->secret = $data['secret'];
+                $localPlugin->identity = $data['identity'];
+                $localPlugin->getConfig()->set('secret', $localPlugin->secret);
+                $localPlugin->getConfig()->set('identity', $localPlugin->identity);
+                $localPlugin->saveConfig();
+                $player->sendMessage(TextFormat::GREEN.'Secret and identity has been set. Try running "/pocketvote diagnose" to confirm that it works.');
+            });
+            $form->setTitle('PocketVote First Time Setup');
+            $form->addLabel('PocketVote is not currently setup. Go to pocketvote.io and sign up, then add your server and go to the settings tab. Once there click the show secrets button and copy both messages into the boxes below.');
+            $form->addInput('Secret', 'Paste secret...', '', 'secret');
+            $form->addInput('Identity', 'Paste identity...', '', 'identity');
+            $event->getPlayer()->sendForm($form);
+        }
         if(PocketVote::$hasVRC) $this->vm->scheduleVRCTask($event->getPlayer()->getName()); # TODO: This should be possible to disable and only allow through commands.
         if(!$this->vm->hasVotes($event->getPlayer()->getName())) return;
 
@@ -62,12 +87,12 @@ class VoteListener implements Listener {
                 $this->vm->removeVote($key);
                 continue;
             }
-            $this->plugin->getServer()->getPluginManager()->callEvent(new VoteDispatchEvent($this->plugin, $vote['player'], $vote['ip'], $vote['site']));
+            $voteDispatchEvent = new VoteDispatchEvent($this->plugin, $vote['player'], $vote['ip'], $vote['site']);
+            $voteDispatchEvent->call();
+
             # Iterate all commands.
             foreach($this->plugin->cmdos as $cmd) {
-                $cmd = str_replace('%player', $vote['player'], $cmd);
-                $cmd = str_replace('%ip', $vote['ip'], $cmd);
-                $cmd = str_replace('%site', $vote['site'], $cmd);
+                $cmd = str_replace(['%player', '%ip', '%site'], [$vote['player'], $vote['ip'], $vote['site']], $cmd);
                 $this->plugin->getServer()->dispatchCommand($sender, $cmd);
             }
             $this->vm->removeVote($key);
